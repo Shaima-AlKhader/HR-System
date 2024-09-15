77bfd2ba0a882e2e9cf65c12b6d136f3955d16ad
Shaymaa AlKhader <87859824+Shaima-AlKhader@users.noreply.github.com>
2024-09-15 10:52:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication4.DSConn;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyConn")));
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod());
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseCors("AllowAllOrigins");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
